3c96de64d0a1504ca9b84f153a5cd74e140875ad
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2023-04-19 11:35:13 +0300

    Add a X button to Expanded nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,9 +12,16 @@ export const Nav = ({ winSize }) => {
                 <>
                     <button className={styles.navBtn} onClick={() => { setExpanded(!isExpanded); }}>
                         {/* get from heroicons.com */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-                        </svg>
+                        {!isExpanded ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        )}
+
                     </button>
                     {isExpanded ? <div>
                         <Link className={styles.resNav} to="/">Home</Link>
