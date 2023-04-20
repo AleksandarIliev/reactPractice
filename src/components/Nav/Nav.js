13de00cde06e9a14c39f581ac319c567867e0a12
Aleksandar Iliev <aleksandar.m.iliev@abv.bg>
2023-04-20 07:53:10 +0300

    Fix a little bug in css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
-import styles from './Nav.module.css'
+import styles from "./Nav.module.css";
 import { Link } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
+import { UserNav } from "./UserNav";
 
 
 export const Nav = ({ winSize }) => {
@@ -10,19 +11,19 @@ export const Nav = ({ winSize }) => {
         <>
             {winSize[0] < 600 ?
                 <>
-                    <button className={styles.navBtn} onClick={() => { setExpanded(!isExpanded); }}>
+                    <button className={styles.navBtn} onClick={() => { setExpanded(!isExpanded) }}>
                         {/* get from heroicons.com */}
                         {!isExpanded ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                             </svg>
                         ) : (
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         )}
-
                     </button>
+
                     {isExpanded ? <div>
                         <Link className={styles.resNav} to="/">Home</Link>
                         <Link className={styles.resNav} to="/products">Products</Link>
@@ -33,14 +34,14 @@ export const Nav = ({ winSize }) => {
                     </div> : []}
                 </>
                 :
-                <div>
-                    <Link className={styles.nav} to="/">Home</Link>
-                    <Link className={styles.nav} to="/products">Products</Link>
-                    <Link className={styles.nav} to="/photoCollection">Photo</Link>
-                    <Link className={styles.nav} to="/rotateBand">Rotate</Link>
-                    <Link className={styles.nav} to="/trafficLight">Traffic</Link>
-                    <Link className={styles.nav} to="/contact">Contact</Link>
-                </div>
+                    <div>
+                        <Link className={styles.nav} to="/">Home</Link>
+                        <Link className={styles.nav} to="/products">Products</Link>
+                        <Link className={styles.nav} to="/photoCollection">Photo</Link>
+                        <Link className={styles.nav} to="/rotateBand">Rotate</Link>
+                        <Link className={styles.nav} to="/trafficLight">Traffic</Link>
+                        <Link className={styles.nav} to="/contact">Contact</Link>
+                    </div>
             }
         </>
     );
